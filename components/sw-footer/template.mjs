54bb2cbb33a9a9745b9d232c6a6bba2eda5015d4
Thon Ly <thonly@heartbank.ceo>
2022-10-28 19:11:39 -0700

    finish footer

--- components/sw-footer/template.mjs
+++ components/sw-footer/template.mjs
@@ -2,8 +2,40 @@ const template = document.createElement("template");
 
 template.innerHTML = `
     <link rel="stylesheet" href="components/sw-footer/shadow.css">
-    link to about instructor;
-    social networks here;
+    <nav>
+        <main>
+            <section>
+                <h5>Donors</h5>
+                <ul class="donors">
+                    <li><a href="https://github.com/thonly"><img src="https://github.com/thonly.png?size=100"></a></li>
+                    <li><a href="https://github.com/panhiathao"><img src="https://github.com/panhiathao.png?size=100"></a></li>
+                    <li><a href="https://github.com/chengsieuly"><img src="https://github.com/chengsieuly.png?size=100"></a></li>
+                    <li><a href="https://github.com/lykimche"><img src="https://github.com/lykimche.png?size=100"></a></li>
+                </ul>
+            </section>
+            <section>
+                <h5>Donate</h5>
+                <ul>
+                    <li><a href="https://heartbank.fund">HeartBank</a></li>
+                </ul>
+                <h5>Contact</h5>
+                <ul>
+                    <li><a href="mailto:market@nhiakou.org">market@nhiakou.org</a></li>
+                </ul>
+            </section>
+            <section>
+                <h5>Connect</h5>
+                <ul>
+                    <li><a href="https://talk.siliconwat.org">Silicon Wat</a></li>
+                    <li><a href="https://www.facebook.com/NhiaKou-101382402038310">Facebook</a></li>
+                    <li><a href="https://medium.com/@nhiakou">Medium</a></li>
+                </ul>
+            </section>
+        </main>
+        <footer>
+            <small><a href="https://nhiakou.com">Nhia Kou</a> &copy; 2023</small>
+        </footer>
+    </nav>
 `;
 
 export default template;
--- components/sw-header/template.mjs
+++ components/sw-header/template.mjs
@@ -5,7 +5,7 @@ template.innerHTML = `
     <header>
         <section>
             <a href="/"><figure>🏪</figure></a>
-            <a href="/"><h1>Nhia Kou Markets</h1></a>
+            <a href="/"><h1>Nhia Kou Market</h1></a>
         </section>
         <input id="menu-toggle" type="checkbox">
         <label class='menu-button-container' for="menu-toggle">
@@ -14,81 +14,55 @@ template.innerHTML = `
         <nav>
             <ul>
                 <li>
-                    <h3>Cambodia</h3>
+                    <h3>Stores</h3>
                     <div>
                         <a href="#backend-cohort"><button>333.eco</button></a>
-                        <h4>Wallet</h4>
+                        <h4>Shoppers / Buyers</h4>
                         <menu>
                             <li><a>Watch Videos on Udemy</a></li>
                             <li><a>Read Textbook on Medium</a></li>
                             <li><a href="#backend-quiz">Take Quizzes to Check Understanding</a></li>
                         </menu>
-                        <h4>Personal Identification</h4>
+                        <h4>Pro Data Entry</h4>
                         <menu>
-                            <li><a href="#backend-podcast">Podcast: Motivation & Coding Music</a></li>
-                            <li><a href="#backend-code">Interactive Coding Exercises</a></li>
-                            <li><a>Remote Pair Programming on Discord</a></li>
-                            <li><a>Students' Solo Projects on Replit</a></li>
-                        </menu>
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <h3>Ukraine</h3>
-                    <div>
-                        <a href="#coupon"><button>Kiitos.Earth</button></a>
-                        <h4>Inclusion & Diversity</h4>
-                        <menu>
-                            <li><a>Women & Minorities</a></li>
-                            <li><a>Ukrainian Refugees</a></li>
-                            <li><a>Cambodian Genocide</a></li>
-                        </menu>
-                        <h4>Campuses</h4>
-                        <menu>
-                            <li><a>Cambodia</a></li>
-                            <li><a>Ukraine</a></li>
-                            <li><a>United States</a></li>
-                        </menu>
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <h3>United States</h3>
-                    <div>
-                        <a href="#ios-cohort"><button>HeartBank.Net</button></a>
-                        <h4>Explorer</h4>
-                        <menu>
-                            <li><a>Watch Videos on Udemy</a></li>
+                            <li><a>Data on what to grow</a></li>
                             <li><a>Read Textbook on Medium</a></li>
                             <li><a href="#ios-quiz">Take Quizzes to Check Understanding</a></li>
                         </menu>
-                        <h4>Supra Blockchain</h4>
+                        <h4>Jobs</h4>
                         <menu>
                             <li><a href="#ios-podcast">Podcast: Motivation & Coding Music</a></li>
                             <li><a href="#ios-code">Interactive Coding Exercises</a></li>
                             <li><a>Remote Pair Programming on Discord</a></li>
                             <li><a>Students' Solo Projects on GitHub</a></li>
                         </menu>
-                        <h4>Infra Blockchains</h4>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h3>Orchards</h3>
+                    <div>
+                        <a href="#coupon"><button>Kiitos.Earth</button></a>
+                        <h4>Decentralized Family Farms</h4>
                         <menu>
-                            <li><a>Live Demo: Music Apps</a></li>
-                            <li><a>Syntax Flashcards</a></li>
-                            <li><a>Students' Writings on Medium</a></li>
-                            <li><a>Mock Interviews on YouTube</a></li>
+                            <li><a>Smart Recommendations</a></li>
+                            <li><a>Zero Waste</a></li>
+                        </menu>
+                        <h4>Vendors / Sellers</h4>
+                        <menu>
+                            <li><a>#Grow-a-Thon</a></li>
+                            <li><a>Certified Local</a></li>
                         </menu>
                     </div>
                 </li>
             </ul>
         </nav>
         <aside>
-            <select onchange="this.getRootNode().host.changeLanguage(event)">
-                <option value="en">English</option>
-                <option value="km" disabled>Khmer</option>
-                <option value="es" disabled>Spanish</option>
-                <option value="ru" disabled>Russian</option>
-                <option value="zh" disabled>Chinese</option>
+            <select onchange="changeLanguage(event)">
+                <option value="kh">Cambodia</option>
+                <option value="ua" disabled>Ukraine</option>
+                <option value="us" disabled>United States</option>
             </select>
         </aside>
     </header>
