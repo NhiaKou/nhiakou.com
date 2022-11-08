15a6848f523329e460abec3ed95580036179c09a
Thon Ly <thonly@heartbank.ceo>
2022-11-08 11:27:35 -0800

    refactor stealth

--- components/sw-header/template.mjs
+++ components/sw-header/template.mjs
@@ -11,64 +11,58 @@ template.innerHTML = `
         <label class='menu-button-container' for="menu-toggle">
             <span class='menu-button'></span>
         </label>
-        <nav>
-            <ul>
-                <li>
-                    <h3>Veggie Seed Bank</h3>
-                    <div>
-                        <a><button>Free Delivery</button></a>
-                        <!--<h4>Learn Horticulture</h4>
-                        <menu>
-                            <li><a>Smart Recommendations</a></li>
-                            <li><a>Zero Waste</a></li>
-                        </menu>
-                        <h4>Vendors / Sellers</h4>
-                        <menu>
-                            <li><a>#Grow-a-Thon</a></li>
-                            <li><a>Certified Local</a></li>
-                        </menu>-->
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <h3>Fruit Tree Nursury</h3>
-                    <div>
-                        <a><button>Grow-a-Thon</button></a>
-                        <!--<h4>Learn Arboriculture</h4>
-                        <menu>
-                            <li><a>Smart Recommendations</a></li>
-                            <li><a>Zero Waste</a></li>
-                        </menu>
-                        <h4>Vendors / Sellers</h4>
-                        <menu>
-                            <li><a>#Grow-a-Thon</a></li>
-                            <li><a>Certified Local</a></li>
-                        </menu>-->
-                    </div>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <h3>Homesteads</h3>
-                    <div>
-                        <a><button>Apply</button></a>
-                        <!--<h4>Decentralized Family Farms</h4>
-                        <menu>
-                            <li><a>Smart Recommendations</a></li>
-                            <li><a>Zero Waste</a></li>
-                        </menu>
-                        <h4>Vendors / Sellers</h4>
-                        Learn Agriculture
-                        Add SW Campus?
-                        <menu>
-                            <li><a>#Grow-a-Thon</a></li>
-                            <li><a>Certified Local</a></li>
-                        </menu>-->
-                    </div>
-                </li>
-            </ul>
-        </nav>
+        <ul>
+            <li>
+                <h3>Veggie Seed Bank</h3>
+                <nav>
+                    <a><button>Free Delivery</button></a>
+                    <!--<h4>Learn Horticulture</h4>
+                    <menu>
+                        <li><a>Smart Recommendations</a></li>
+                        <li><a>Zero Waste</a></li>
+                    </menu>
+                    <h4>Vendors / Sellers</h4>
+                    <menu>
+                        <li><a>#Grow-a-Thon</a></li>
+                        <li><a>Certified Local</a></li>
+                    </menu>-->
+                </nav>
+            </li>
+            <li>
+                <h3>Fruit Tree Nursury</h3>
+                <nav>
+                    <a><button>Grow-a-Thon</button></a>
+                    <!--<h4>Learn Arboriculture</h4>
+                    <menu>
+                        <li><a>Smart Recommendations</a></li>
+                        <li><a>Zero Waste</a></li>
+                    </menu>
+                    <h4>Vendors / Sellers</h4>
+                    <menu>
+                        <li><a>#Grow-a-Thon</a></li>
+                        <li><a>Certified Local</a></li>
+                    </menu>-->
+                </nav>
+            </li>
+            <li>
+                <h3>Homesteads</h3>
+                <nav>
+                    <a><button>Apply</button></a>
+                    <!--<h4>Decentralized Family Farms</h4>
+                    <menu>
+                        <li><a>Smart Recommendations</a></li>
+                        <li><a>Zero Waste</a></li>
+                    </menu>
+                    <h4>Vendors / Sellers</h4>
+                    Learn Agriculture
+                    Add SW Campus?
+                    <menu>
+                        <li><a>#Grow-a-Thon</a></li>
+                        <li><a>Certified Local</a></li>
+                    </menu>-->
+                </nav>
+            </li>
+        </ul>
         <aside>
             <select onchange="this.getRootNode().host.changeCountry(event)">
                 <option value="kh">Cambodia</option>
