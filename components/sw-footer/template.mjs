681b646ebca2cdce9f61d7fd61547742fcbc8ab6
Thon Ly <thonly@heartbank.ceo>
2022-11-09 17:33:57 -0800

    add svg background

--- components/sw-footer/template.mjs
+++ components/sw-footer/template.mjs
@@ -5,31 +5,45 @@ template.innerHTML = `
     <nav>
         <main>
             <section>
-                <h5>Donors</h5>
-                <ul class="donors">
-                    <li><a href="https://github.com/thonly"><img src="https://github.com/thonly.png?size=100"></a></li>
-                    <li><a href="https://github.com/panhiathao"><img src="https://github.com/panhiathao.png?size=100"></a></li>
-                    <li><a href="https://github.com/chengsieuly"><img src="https://github.com/chengsieuly.png?size=100"></a></li>
-                    <li><a href="https://github.com/lykimche"><img src="https://github.com/lykimche.png?size=100"></a></li>
-                </ul>
+                <div>
+                    <h5>Donors</h5>
+                    <ul class="donors">
+                        <li><a href="https://github.com/thonly"><img src="https://github.com/thonly.png?size=100"></a></li>
+                        <li><a href="https://github.com/panhiathao"><img src="https://github.com/panhiathao.png?size=100"></a></li>
+                        <li><a href="https://github.com/chengsieuly"><img src="https://github.com/chengsieuly.png?size=100"></a></li>
+                        <li><a href="https://github.com/lykimche"><img src="https://github.com/lykimche.png?size=100"></a></li>
+                    </ul>
+                </div>
             </section>
             <section>
-                <h5>Donate</h5>
-                <ul>
-                    <li><a href="https://heartbank.fund">HeartBank</a></li>
-                </ul>
-                <h5>Contact</h5>
-                <ul>
-                    <li><a href="mailto:orchard@nhiakou.org">orchard@nhiakou.org</a></li>
-                </ul>
+                <div>
+                    <h5>Thank</h5>
+                    <ul>
+                        <li><a href="https://kiitos.earth">Kiitos</a></li>
+                    </ul> 
+                </div>
+                <div>
+                    <h5>Donate</h5>
+                    <ul>
+                        <li><a href="https://heartbank.fund">HeartBank</a></li>
+                    </ul>
+                </div>
             </section>
             <section>
-                <h5>Connect</h5>
-                <ul>
-                    <li><a href="https://talk.siliconwat.org">Silicon Wat</a></li>
-                    <li><a href="https://www.facebook.com/NhiaKou-101382402038310">Facebook</a></li>
-                    <li><a href="https://medium.com/@nhiakou">Medium</a></li>
-                </ul>
+                <div>
+                    <h5>Connect</h5>
+                    <ul>
+                        <li><a href="https://talk.siliconwat.org">Silicon Wat</a></li>
+                        <li><a href="https://www.facebook.com/NhiaKou-101382402038310">Facebook</a></li>
+                        <li><a href="https://medium.com/@nhiakou">Medium</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>Contact</h5>
+                    <ul>
+                        <li><a href="mailto:orchard@nhiakou.org">orchard@nhiakou.org</a></li>
+                    </ul> 
+                </div>
             </section>
         </main>
         <footer>
