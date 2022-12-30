4d7098a3ba70264c177c988e58202057e16f1bcd
Thon Ly <thonly@heartbank.ceo>
2022-12-29 21:19:55 -0800

    refactor global

--- components/nk-footer/nk-donors/template.mjs
+++ components/nk-footer/nk-donors/template.mjs
@@ -1,4 +1,4 @@
-import { ORIGIN, THONLY } from "/global.mjs";
+import { ORCHARDS as ORIGIN, THONLY } from "https://thonly.org/global.mjs";
 const template = document.createElement("template");
 
 template.innerHTML = `
--- global.mjs
+++
@@ -1,2 +0,0 @@
-export const ORIGIN = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5850" : "https://nhiakou.org";
-export const THONLY = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5500" : "https://thonly.org";
